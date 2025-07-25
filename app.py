3884df6835b57e3cd62c504da7161c9c45976ec6
Gilbert Laman <38028534+GPL-PRC0977@users.noreply.github.com>
2025-07-25 11:43:39 +0800

    Use the backup code

--- app.py
+++ app.py
@@ -9,20 +9,19 @@ import json
 app = Flask(__name__)
 
 load_dotenv()
+api_key_secrets = os.getenv("API_CRED_KEYS")
+api_project_id = os.getenv("API_PROJECT_ID")
 
-bq_cred = os.getenv("bq_cred")
-bq_project_id = os.getenv("bq_project_id")
-
-ep_key_table_id = os.getenv("ep_table_id")
-ep_key_project_id = os.getenv("ep_project_id")
-ep_key_secret_id = os.getenv("ep_secret_id")
+api_key_table_id = os.getenv("api_key_table_id")
+api_key_project_id = os.getenv("api_key_project_id")
+api_key_secret_id = os.getenv("api_key_secret_id")
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_credentials_from_secret(project_id: str, secret_id: str):
     secret_client = secretmanager.SecretManagerServiceClient()
-    secret_name = f"projects/{project_id}/secrets/{secret_id}/versions/1"
+    secret_name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
 
     response = secret_client.access_secret_version(
         request={"name": secret_name})
@@ -34,20 +33,43 @@ def get_credentials_from_secret(project_id: str, secret_id: str):
 
 
 api_key_credentials = get_credentials_from_secret(
-    bq_project_id, bq_cred)
+    api_project_id, api_key_secrets)
+
+# api_key_credentials2 = get_credentials_from_secret(
+#     api_key_project_id, api_key_secret_id)
+
+# key = os.path.join(current_directory, 'pgc-dma-dev-sandbox.json')
+# credentials = service_account.Credentials.from_service_account_file(
+#     key, scopes=["https://www.googleapis.com/auth/cloud-platform",
+#                  "https://spreadsheets.google.com/feeds",
+#                  "https://www.googleapis.com/auth/spreadsheets",
+#                  "https://www.googleapis.com/auth/drive.file",
+#                  "https://www.googleapis.com/auth/drive"],
+# )
 
 client = bigquery.Client(credentials=api_key_credentials,
                          project=api_key_credentials.project_id)
 
+# API_KEY = os.getenv("API_KEY")
+
+
+# def require_api_key(f):
+#     def decorated_function(*args, **kwargs):
+#         api_key = request.headers.get('X-API-KEY')
+#         if api_key != API_KEY:
+#             return jsonify({"error": "Unauthorized"}), 401
+#         return f(*args, **kwargs)
+#     return decorated_function
+
 
 def is_valid_api_key(api_key):
     api_key_credentials = get_credentials_from_secret(
-        ep_key_project_id, ep_key_secret_id)
+        api_key_project_id, api_key_secret_id)
     api_client = bigquery.Client(
-        credentials=api_key_credentials, project=ep_key_project_id)
+        credentials=api_key_credentials, project=api_key_project_id)
 
     query = f"""
-        SELECT 1 FROM `{ep_key_table_id}`
+        SELECT 1 FROM `{api_key_table_id}`
         WHERE api_key = @api_key
         AND active = 1
         LIMIT 1
@@ -134,7 +156,3 @@ def get_product():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# if __name__ == '__main__':
-#     port = int(os.environ.get('PORT', 8080))
-#     app.run(host='0.0.0.0', port=port)
